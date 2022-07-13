f66f1c4ea39f2f98bdfae3690a9c3d6b62c1bdc6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-07-13 17:48:50 +1200

    starting on layout

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Plurigrid</h3>
+        The future of pluralistic grids is here.
+      </header>
+    </div>
+  );
+}
+
+export default App;
