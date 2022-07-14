bf2d1cf5c5461ad21d9cc07dd811c657dace7c54
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-07-14 21:59:46 +1200

    add active state

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export function Header() {
+  let navigate = useNavigate();
   return (
     <header>
       <div className="menu">
-        <Link to="/" className="menu-item"><div>Plurigrid</div></Link>
-        <Link to="/matrix" className="menu-item"><div>$MATRIX</div></Link>
-        <Link to="/hatchers" className="menu-item"><div>Hatchers</div></Link>
-        <Link to="/simulation" className="menu-item"><div>Simulation</div></Link>
+        <NavLink to="/" className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}><div>Plurigrid</div></NavLink>
+        <NavLink to="/matrix" className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}><div>$MATRIX</div></NavLink>
+        <NavLink to="/hatchers" className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}><div>Hatchers</div></NavLink>
+        <NavLink to="/simulation" className={({ isActive }) => isActive ? "menu-item active" : "menu-item"}><div>Simulation</div></NavLink>
       </div>
     </header>
   );
