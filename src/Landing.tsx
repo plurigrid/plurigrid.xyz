d919b7f6a009252301adff15a389773ef254a20e
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-07-16 23:53:36 +1200

    add a container and media queries

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -14,7 +14,7 @@ function Landing() {
         <img src="/logo.png" className="logo" height={300} />
         <div className="lead-explainer">
           <h1>Plurigrid</h1>
-          Plurigrid is building a powerful tool that allows you to own a vertex of an energy grid that guarantees a stable, resilient clean energy supply to your home or business.
+          <p>Plurigrid is building a powerful tool that allows you to own a vertex of an energy grid that guarantees a stable, resilient clean energy supply to your home or business.</p>
 
           <div className="socials-container">
             <Socials />
@@ -29,38 +29,38 @@ function Landing() {
         {/* Plurigrid will instantiate access to the basic right to the public good of sustainable energy. This tool offers an immediate and fair response to any extraneous energy needs-- and bypasses inefficiencies due to cumbersome appeals to your local energy operators-- using blockchain technology that gives you a substantial say in how your local grid is governed. */}
       </div>
       <div className="lead">
-        <h1>Vision</h1>
-
-        Energy coordination and baseline level of access to electricity is a human right ⚡️🇺🇳
-
-        <div className="vision-grid">
-          <div className="vision-item">
-            <img src="noun-capital-2842004.svg" width={80} />
-            <div className="vision-item-explainer">
-              <div className="vision-item-title">Capital efficiency</div>
-              <div>As a microgrid powered by solar &mdash; can&nbsp;I go to the market and be sure that I am optimally allocating every $ </div>
+        <div className="vision-container">
+          <h1>Plurigrid's vision</h1>
+          <p>Energy coordination and baseline level of access to electricity is a human right ⚡️🇺🇳</p>
+          <div className="vision-grid">
+            <div className="vision-item">
+              <img src="noun-capital-2842004.svg" width={80} />
+              <div className="vision-item-explainer">
+                <div className="vision-item-title">Capital efficiency</div>
+                <div>As a microgrid powered by solar &mdash; can&nbsp;I go to the market and be sure that I am optimally allocating every $ </div>
               </div>
-          </div>
-          <div className="vision-item">
-            <img src="noun-moral-901639.svg" width={80} />
-            <div className="vision-item-explainer">
-              <div className="vision-item-title">Value alignment</div>
-              <div>Not violating ethical standards of microgrid users while still coordinating in the grid</div>
+            </div>
+            <div className="vision-item">
+              <img src="noun-moral-901639.svg" width={80} />
+              <div className="vision-item-explainer">
+                <div className="vision-item-title">Value alignment</div>
+                <div>Not violating ethical standards of microgrid users while still coordinating in the grid</div>
               </div>
-          </div>
-          <div className="vision-item">
-            <img src="noun-domino-effect-4450396.svg" width={80} />
-            <div className="vision-item-explainer">
-              <div className="vision-item-title">Resilience</div>
-              <div>In an event of a transmission failure&#xfeff;&mdash;&#xfeff;climate, war, political upheaval&#xfeff;&mdash;&#xfeff;the grid of grids needs to rapidly reconfigure itself or risk an outage</div>
+            </div>
+            <div className="vision-item">
+              <img src="noun-domino-effect-4450396.svg" width={80} />
+              <div className="vision-item-explainer">
+                <div className="vision-item-title">Resilience</div>
+                <div>In an event of a transmission failure&#xfeff;&mdash;&#xfeff;climate, war, political upheaval&#xfeff;&mdash;&#xfeff;the grid of grids needs to rapidly reconfigure itself or risk an outage</div>
               </div>
-          </div>
-          <div className="vision-item">
-            <img src="noun-stealing-money-2060045.svg" width={80} />
-            <div className="vision-item-explainer">
-              <div className="vision-item-title">Fraud prevention</div>
-              <div>Shift the enforcement of agreed upon energy distribution rules from human intermediaries / operators to math and code under certain well-understood corruptibility barriers</div>
+            </div>
+            <div className="vision-item">
+              <img src="noun-stealing-money-2060045.svg" width={80} />
+              <div className="vision-item-explainer">
+                <div className="vision-item-title">Fraud prevention</div>
+                <div>Shift the enforcement of agreed upon energy distribution rules from human intermediaries / operators to math and code under certain well-understood corruptibility barriers</div>
               </div>
+            </div>
           </div>
         </div>
 
